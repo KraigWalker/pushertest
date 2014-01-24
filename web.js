f3d46f42a93bb53735cb1e4209a1890bb82fcea1
Kraig Walker <kraig_walker@me.com>
2014-01-24 22:07:56 +0000

    add pusher code

--- web.js
+++ web.js
@@ -1,12 +1,21 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+var Pusher = require('pusher');
 var app = express();
+
+var pusher = new Pusher({
+  appId: '64386',
+  key: '5f0a592328baabc35293',
+  secret: '2fe4afb1303c2e2f88f2'
+});
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.sendfile('index.html');
+
+  pusher.trigger('my-channel', 'my-event', {"message": "hello world"});
 });
 
 var port = Number(process.env.PORT || 5000);
