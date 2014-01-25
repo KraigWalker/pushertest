35f7b6f9ad1967fe1c10607d3d53eddc024843b8
Kraig Walker <kraig_walker@me.com>
2014-01-25 14:02:03 +0000

    improve server middleware

--- web.js
+++ web.js
@@ -5,15 +5,33 @@ var app = express(),
  server = http.createServer(app),
   io = require('socket.io').listen(server);
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+  app.use(function(req, res, next) {
+      var oneof = false;
+      if(req.headers.origin) {
+          res.header('Access-Control-Allow-Origin', req.headers.origin);
+          oneof = true;
+      }
+      if(req.headers['access-control-request-method']) {
+          res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+          oneof = true;
+      }
+      if(req.headers['access-control-request-headers']) {
+          res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+          oneof = true;
+      }
+      if(oneof) {
+          res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+      }
 
-    next();
-};
+      // intercept OPTIONS method
+      if (oneof && req.method == 'OPTIONS') {
+          res.send(200);
+      }
+      else {
+          next();
+      }
+  });
 
-app.use(allowCrossDomain);
 
 server.listen(process.env.PORT || 5000);
 
