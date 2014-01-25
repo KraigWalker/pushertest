d4d92c0d2d3576bcefeef3a28d68284f50f8b036
Kraig Walker <kraig_walker@me.com>
2014-01-25 09:13:24 +0000

    app key over app id

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var pusher = new Pusher('64386');
+var pusher = new Pusher('5f0a592328baabc35293');
 var channel = pusher.subscribe('my-channel');
 channel.bind('my-event', function(data) {
 	alert('An event was triggered with message: ' + data.message);
