f289ace576586ee31dc01551396c67650d8a29a9
Kraig Walker <kraig_walker@me.com>
2014-01-25 11:19:09 +0000

    add authentication

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var pusher = new Pusher('5f0a592328baabc35293');
+var pusher = new Pusher('5f0a592328baabc35293', { authEndpoint: 'http://mighty-escarpment-2260.herokuapp.com/pusher/auth'});
 var channel = pusher.subscribe('presence-channel');
 // handle success
 channel.bind('pusher:subscription_succeeded', function() {
