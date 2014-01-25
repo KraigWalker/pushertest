eaa9457f8d044b765a4e1ebd2589aaca05354999
Kraig Walker <kraig_walker@me.com>
2014-01-25 11:42:23 +0000

    use presence and client channels

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var pusher = new Pusher('5f0a592328baabc35293', { authEndpoint: 'http://mighty-escarpment-2260.herokuapp.com/pusher/auth'});
-var channel = pusher.subscribe('my-channel');
+var channel = pusher.subscribe('presence-my-channel');
 // handle success
 channel.bind('pusher:subscription_succeeded', function() {
 	alert("subscription successful");
@@ -9,12 +9,12 @@ channel.bind('pusher:subscription_error', function() {
 	alert("subscription error!");
 });
 
-channel.bind("presense-update-slider", function(data){
+channel.bind("client-update-slider", function(data){
 	alert("value " + data.value);
 });
 
 // send a message with the amount to the server
 function updateSlider() {
-	channel.trigger("presense-update-slider", { value:45 });
+	channel.trigger("client-update-slider", { value:45 });
 	console.log("event sent");
 }
