ca6175a0280abacd631041d4ce5f25debbbb0850
Kraig Walker <kraig_walker@me.com>
2014-01-25 14:03:46 +0000

    improve server middleware

--- web.js
+++ web.js
@@ -5,33 +5,11 @@ var app = express(),
  server = http.createServer(app),
   io = require('socket.io').listen(server);
 
-  app.use(function(req, res, next) {
-      var oneof = false;
-      if(req.headers.origin) {
-          res.header('Access-Control-Allow-Origin', req.headers.origin);
-          oneof = true;
-      }
-      if(req.headers['access-control-request-method']) {
-          res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
-          oneof = true;
-      }
-      if(req.headers['access-control-request-headers']) {
-          res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
-          oneof = true;
-      }
-      if(oneof) {
-          res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
-      }
-
-      // intercept OPTIONS method
-      if (oneof && req.method == 'OPTIONS') {
-          res.send(200);
-      }
-      else {
-          next();
-      }
+  app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
   });
-
 
 server.listen(process.env.PORT || 5000);
 
