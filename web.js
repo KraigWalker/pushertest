64207073d8f84d788d45914ef73f504c0f92922b
Kraig Walker <kraig_walker@me.com>
2014-01-25 14:20:14 +0000

    ugh

--- web.js
+++ web.js
@@ -1,13 +1,16 @@
 var express = require('express'),
-http = require('http'),
-cors = require('cors');
-
+http = require('http');
 var app = express(),
 server = http.createServer(app),
 io = require('socket.io').listen(server);
 
-  app.use(cors());
+var enableCORS = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+};
 
+app.use(enableCORS);
 server.listen(process.env.PORT || 5000);
 
 app.get('/', function (req, res) {
