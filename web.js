515db986dd2cf02c0007bf405fe322e10c0fae1f
Kraig Walker <kraig_walker@me.com>
2014-01-25 13:53:33 +0000

    allow cross domain

--- web.js
+++ web.js
@@ -5,7 +5,17 @@ var app = express(),
  server = http.createServer(app),
   io = require('socket.io').listen(server);
 
-server.listen(process.env.PORT || 5000)
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', config.allowedDomains);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+};
+
+app.use(allowCrossDomain);
+
+server.listen(process.env.PORT || 5000);
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
