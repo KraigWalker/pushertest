16596215e636c42e910ef419d157c16d4abba2cf
Kraig Walker <kraig_walker@me.com>
2014-01-25 12:03:49 +0000

    remove authendpoint from client

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var pusher = new Pusher('5f0a592328baabc35293', { authEndpoint: 'http://mighty-escarpment-2260.herokuapp.com/pusher/auth'});
+var pusher = new Pusher('5f0a592328baabc35293');
 var channel = pusher.subscribe('presence-my-channel');
 // handle success
 channel.bind('pusher:subscription_succeeded', function() {
