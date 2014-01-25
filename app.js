2de83074fcbc94b1b88fdb5a61ecc090cd6033ff
Kraig Walker <kraig_walker@me.com>
2014-01-25 10:33:51 +0000

    use app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var pusher = new Pusher('5f0a592328baabc35293');
 var channel = pusher.subscribe('my-channel');
-channel.bind('my-event', function(data) {
-	alert('An event was triggered with message: ' + data.message);
-});
+	// send a message with the amount to the server
+function updateSlider() {
+	var triggered = channel.trigger('update-slider', {"value": "45"});
+}
