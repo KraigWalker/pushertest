0a958752235f73e0da1c65fa4051dd257e787a92
Kraig Walker <kraig_walker@me.com>
2014-01-25 10:53:22 +0000

    change object data of pushes

--- js/app.js
+++ js/app.js
@@ -2,5 +2,6 @@ var pusher = new Pusher('5f0a592328baabc35293');
 var channel = pusher.subscribe('my-channel');
 	// send a message with the amount to the server
 function updateSlider() {
-	var triggered = channel.trigger('update-slider', {"value": "45"});
+	var triggered = channel.trigger('update-slider', { value: 45 });
+
 }
