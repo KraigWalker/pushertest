678b2bc864e17c059a74698101d6df0c10e22a03
Kraig Walker <kraig_walker@me.com>
2014-01-25 13:58:34 +0000

    allow all domains

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express(),
   io = require('socket.io').listen(server);
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', config.allowedDomains);
+    res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
