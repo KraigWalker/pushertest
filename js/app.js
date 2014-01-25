da6408d385de584bf94efacc765eccc7f0ea89ef
Kraig Walker <kraig_walker@me.com>
2014-01-25 12:17:12 +0000

    switch for socket io

--- js/app.js
+++ js/app.js
@@ -1,20 +1,8 @@
-var pusher = new Pusher('5f0a592328baabc35293');
-var channel = pusher.subscribe('presence-my-channel');
-// handle success
-channel.bind('pusher:subscription_succeeded', function() {
-	alert("subscription successful");
-});
-// handle error
-channel.bind('pusher:subscription_error', function() {
-	alert("subscription error!");
-});
-
-channel.bind("client-update-slider", function(data){
-	alert("value " + data.value);
-});
-
-// send a message with the amount to the server
-function updateSlider() {
-	channel.trigger("client-update-slider", { value:45 });
-	console.log("event sent");
-}
+<script src="/socket.io/socket.io.js"></script>
+<script>
+  var socket = io.connect('http://localhost');
+  socket.on('news', function (data) {
+    console.log(data);
+    socket.emit('my other event', { my: 'data' });
+  });
+</script>
--- web.js
+++ web.js
@@ -1,63 +1,15 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
-var Pusher = require('pusher-client');
+var io = require('socket.io').listen(80);
 var app = express();
 
-var appId = '64386';
-var key = '5f0a592328baabc35293';
-var secret = '2fe4afb1303c2e2f88f2';
-var allowedHost = process.env.ALLOWED_HOST;
-
-var pusher = new Pusher( { appId: appId, key: key, secret: secret } );
-
-var app = express( express.logger() );
-app.use( express.bodyParser() );
-
-var allowCrossDomain = function(req,res, next) {
-	res.header('Access-Control-Allow-Origin', allowedHost);
-	next();
-};
-app.use( allowCrossDomain );
-
-app.post('/pusher/auth', function(request, response) {
-
-	console.log(request.body);
-
-	var channelName = request.body.channel_name;
-	var socketId = request.body.socket_id;
-	var user = JSON.parse( request.body.user );
-
-	var channnelData;
-
-  if(channelName.indexOf('private-') === 0) {
-    channelData = null;
-  }
-  else if(channelName.indexOf('presence-') === 0) {
-    channelData = {
-      user_id: user.id,
-      user_info: { displayName: user.displayName }
-    };
-  }
-  else {
-    response.end();
-  }
-
-  var auth = pusher.auth(socketId, channelName, channelData);
-  console.log(auth);
-  response.send(auth);
-
+io.sockets.on('connection', function (socket) {
+	socket.emit('news', { hello: 'world' });
+	socket.on('my other event', function (data) {
+		console.log(data);
+	});
 });
-
-
-app.get('/', function(req, res) {
-  res.sendfile('index.html');
-});
-app.get('/js/app.js', function(req, res) {
-	res.sendfile('js/app.js');
-});
-
-
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
