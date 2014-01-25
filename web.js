4ce79e0f8b563a456950fdf595aa99c49ea1387a
Kraig Walker <kraig_walker@me.com>
2014-01-25 13:49:18 +0000

    use heroku port first

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express(),
  server = http.createServer(app),
   io = require('socket.io').listen(server);
 
-server.listen(10000);
+server.listen(process.env.PORT || 5000)
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
