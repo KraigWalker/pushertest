e4bc5d0c2c14429930f5f497c7eaf8023cd2d78e
Kraig Walker <kraig_walker@me.com>
2014-01-25 12:22:31 +0000

    use socket io example

--- web.js
+++ web.js
@@ -1,17 +1,15 @@
-// web.js
-var express = require("express");
-var logfmt = require("logfmt");
-var io = require('socket.io').listen(80);
-var app = express();
+var app = require('express').createServer(),
+io = require('socket.io').listen(app);
 
-io.sockets.on('connection', function (socket) {
-	socket.emit('news', { hello: 'world' });
-	socket.on('my other event', function (data) {
-		console.log(data);
-	});
+app.listen(80);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
 });
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
