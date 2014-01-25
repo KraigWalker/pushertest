1ec5b18d9f8d2d915ecd9b48bd7c9517a0fc3fee
Kraig Walker <kraig_walker@me.com>
2014-01-25 10:56:10 +0000

    add subscription check

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 var pusher = new Pusher('5f0a592328baabc35293');
 var channel = pusher.subscribe('my-channel');
+channel.bind('pusher:subscription_succeeded', function() {
+	alert("subscription successful!");
+});
 	// send a message with the amount to the server
 function updateSlider() {
 	var triggered = channel.trigger('update-slider', { value: 45 });
