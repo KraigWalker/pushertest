efa9bd9d4d794bf3ec48d2bf16fec1cfd5ca8789
Kraig Walker <kraig_walker@me.com>
2014-01-25 17:26:05 +0000

    try to sort out port issue

--- game.js
+++ game.js
@@ -21,7 +21,10 @@ function init() {
 	players = [];
 
 	// Set up Socket.IO to listen on port 8000
-	socket = io.listen(process.env.PORT || 8000);
+	var port = process.env.PORT || 8000;
+	socket = io.listen(port, function(){
+		console.log("server listening on port %d in %s mode", app.address().port, app.settings.env);
+	});
 
 	// Configure Socket.IO
 	socket.configure(function() {
