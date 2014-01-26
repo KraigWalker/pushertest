a33382cb500f387078ee75d9d5f3de2961fd97b3
Kraig Walker <kraig_walker@me.com>
2014-01-26 02:42:29 +0000

    start using background canvas

--- public/js/game.js
+++ public/js/game.js
@@ -1,23 +1,22 @@
 /**************************************************
 ** GAME VARIABLES
 **************************************************/
-var canvas,			// Canvas DOM element
-	ctx,			// Canvas rendering context
+var ctx,			// Canvas rendering context
 	keys,			// Keyboard input
 	localPlayer,	// Local player
 	remotePlayers,	// Remote players
 	socket;			// Socket connection
 
-	var stage = new createjs.Stage("gameCanvas");
 	var circle = new createjs.Shape();
+	var text = new createjs.Text("Outnumbered", "px Helvetica", "#000000");
+
 
 /**************************************************
 ** GAME INITIALISATION
 **************************************************/
 function init() {
 	// Declare the canvas and rendering context
-	canvas = document.getElementById("gameCanvas");
-	ctx = canvas.getContext("2d");
+	ctx = document.getCSSCanvasContext('2d', 'animation', 300, 300);
 
 	// CreateJS Getting Started
 	circle.graphics.beginFill("red").drawCircle(0, 0, 50);
@@ -25,14 +24,14 @@ function init() {
 	circle.y = 100;
 	circle.addEventListener("click", function(event) {
 	//using HTML5 for fullscreen (only newest Chrome + FF)
-	document.getElementById("gameCanvas").webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT); //Chrome
-	document.getElementById("gameCanvas").mozRequestFullScreen(); //Firefox
 	});
-	stage.addChild(circle);
+	text.x = (window.innerWidth/2);
+	text.y = (window.innerHeight/2);
+	text.textBaseline = "alphabetic";
 
 	// Maximise the canvas
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+	ctx.width = window.innerWidth;
+	ctx.height = window.innerHeight;
 
 	// Initialise keyboard controls
 	keys = new Keys();
@@ -40,8 +39,8 @@ function init() {
 	// Calculate a random start position for the local player
 	// The minus 5 (half a player size) stops the player being
 	// placed right on the egde of the screen
-	var startX = Math.round(Math.random()*(canvas.width-5)),
-		startY = Math.round(Math.random()*(canvas.height-5));
+	var startX = Math.round(Math.random()*(ctx.width-5)),
+		startY = Math.round(Math.random()*(ctx.height-5));
 
 	// Initialise the local player
 	localPlayer = new Player(startX, startY);
@@ -100,8 +99,8 @@ function onKeyup(e) {
 // Browser window resize
 function onResize(e) {
 	// Maximise the canvas
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+	ctx.width = window.innerWidth;
+	ctx.height = window.innerHeight;
 }
 
 // Socket connected
@@ -185,9 +184,7 @@ function update() {
 **************************************************/
 function draw() {
 	// Wipe the canvas clean
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-	stage.update();
+	ctx.clearRect(0, 0, ctx.width, ctx.height);
 
 	// Draw the local player
 	localPlayer.draw(ctx);
